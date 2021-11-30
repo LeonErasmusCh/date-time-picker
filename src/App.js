55a5961870740718decef7bb30898feb222191d8
Leon Erasmus <leonerasmus@Leons-MacBook-Pro.local>
2021-11-30 15:42:10 -0300

    css

--- src/App.js
+++ src/App.js
@@ -21,20 +21,26 @@ function App() {
 
   return (
     <>
-    <div className="container my-5 p-5 text-center bg-light rounded-3 w-75">
-      <h4 className="text-secondary">Please select date and time</h4>
-      <input type="date" onSelect={captureDate}></input>
+    <div className="container my-5 p-5 text-center rounded-3 w-75 pickerBackround">
+      <h4 className="text-light p-3">Select date and time</h4>
+      
+      <input type="date" className="input mx-2" onSelect={captureDate}></input>
 
-      <select id="mySelect" className="m-2 p-1" onChange={captureTime}>
+      <select id="mySelect" className="input time mx-2" onChange={captureTime}>
         <option defaultValue disabled>10:00 to 11:00</option>
         <option  type="text" value="11:00 to 12:00">11:00 to 12:00</option>
         <option value="12:00 to 13:00">12:00 to 13:00</option>
         <option value="13:00 to 14:00">13:00 to 14:00</option>
+        <option value="14:00 to 15:00">13:00 to 14:00</option>
+        <option value="15:00 to 16:00">13:00 to 14:00</option>
+        <option value="16:00 to 17:00">13:00 to 14:00</option>
+        <option value="17:00 to 18:00">13:00 to 14:00</option>
+        <option value="18:00 to 119:00">13:00 to 14:00</option>
       </select>
   
-   <div className="row my-4">
-     <p>Date picked: {date}</p>
-     <p>Time picked: {time}</p>
+   <div className="row my-4 text-light">
+     <h6>Date: {date}</h6>
+     <h6>Time: {time}</h6>
    </div>
    </div>
    </>
